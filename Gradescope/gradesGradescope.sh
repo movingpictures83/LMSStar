ff08e9de6fb409f230252f6a2b97e5b2653314f9
Lawrence Chik <lchik001@fiu.edu>
2020-11-04 11:20:49 -0600

    Ported gradesGradescope.py to gradesGradescope.sh

---
+++ Gradescope/gradesGradescope.sh
@@ -0,0 +1,19 @@
+#Usage: ./gradesGradescope.sh (filename) (outputfile)
+#Example: ./gradesGradescope.sh example/example1.csv testOutput.txt
+#$1 = filename
+#$2 = outputfile
+
+if [ $# -lt 2 -o $# -gt 2 ]
+    then
+        echo "Usage: ./gradesGradescope.sh (filename) (outputfile)";
+        exit 2;
+fi
+
+if (! test -e $1)
+    then
+        echo "FILE DOES NOT EXIST"
+        exit 1
+    else
+        echo "\"Full Name\",\"Score\"" > $2
+        awk -F, '(NR > 0) {printf("%s \t %s \n", $1, $4);}' $1 >> $2
+fi
