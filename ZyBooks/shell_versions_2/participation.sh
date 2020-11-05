912f209208357a12789b8aec3c02c59b519d2ded
rszab003 <70474072+rszab003@users.noreply.github.com>
2020-11-05 23:06:18 +0100

    add usage check for no args

--- ZyBooks/shell_versions_2/participation.sh
+++ ZyBooks/shell_versions_2/participation.sh
@@ -2,11 +2,16 @@
 
 #USAGE: ./participation.sh (classtime file) (examtime file) (outputfile) (data column number) (total points possible)
 
-USAGE="./participation.sh (classtime file) (examtime file) (outputfile) (data co    lumn number) (total points possible)"
-
+USAGE="./participation.sh (classtime file) (examtime file) (outputfile) (data column number) (total points possible) [weight adjustment]"
+#TODO add capability use custom grade weight (eg, 0.75, 0.5, etc
 outFile=$3
 partCol=$4
 totalPoints=$5
+
+if [ $# -eq 0 ]; then
+	echo "No arguments given."
+	echo $USAGE
+fi
 
 if [ -e $1 ]; then
 	classtimeFile=$1
@@ -31,7 +36,6 @@ awk -F, 'NR>1 { print $1,$2,$5 }' $classtimeFile > $outFile
 
 exArrLen=${#examtimeArr[@]}
 classArrLen=${#classtimeArr[@]}
-
 #IF ARRAY LENGTHS DIFFER, THERE MUST BE AN ERROR > EXIT
 if [ $exArrLen -ne $classArrLen ]; then
 	echo "ERROR: Check that input files are for the same class"
@@ -47,7 +51,9 @@ for i in "${examtimeArr[@]}"; do
 	xPercent=$(echo "scale=2;${x}/${totalPoints}" | bc)
 	# append calculated scores to output file 
 	# TODO get this working for macOS
-	sed -i "${lineNum} s/$/ ${x} ${xPercent}/p" $outFile
+	#sed -i "${lineNum} s/$/ ${x} ${xPercent}/p" $outFile
+	y=`cat $outFile`
+	sed "${lineNum} s/$/${x} ${xPercent}/p" $y >> $outFile
 	((index=index+1))
 	((lineNum=lineNum+1))
 done
