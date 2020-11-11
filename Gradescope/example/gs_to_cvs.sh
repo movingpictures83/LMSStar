1723f96d60e4f46e2c1c230f966d778f5e474dc3
Maurice <mauriceb2197@gmail.com>
2020-11-11 02:18:11 -0500

    Added script to convert gradescope to canvas, also added example file to test on and output file

--- Gradescope/example/gs_to_cvs.sh
+++ Gradescope/example/gs_to_cvs.sh
@@ -9,13 +9,6 @@ function rmcol(col, i){
 	}
 	NF--;
 }
-function addcol(col,i){
-	NF++
-	for(i=NF; i>col+1; i--){
-		$i = $(i-1);	
-	}
-	$col="ID";
-}
 {
 	#Converting emails in gradescope to SIS Login ID
 	if((x=index($3, "@")) > 0){
@@ -23,10 +16,13 @@ function addcol(col,i){
 	}
 	#Converting column names to Canvas column names
 	if(FNR == 1){
-		$1="Student";
-		$2="SIS User ID";
-		$3="SIS Login ID";
+		$1 ="Student";
+		$2 ="SIS User ID";
+		$NF = "Final Score";
+	}else{
+		$NF=$4
 	}
+	rmcol(4);
 	print $0;
 	
 	
