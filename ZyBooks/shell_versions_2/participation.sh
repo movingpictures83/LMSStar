13278180f088426164930fe1abb8bdace391d8cb
Robert Szabo <rszab003@fiu.edu>
2020-12-02 17:21:17 +0100

    fixed participation.sh to use awk only

--- ZyBooks/shell_versions_2/participation.sh
+++ ZyBooks/shell_versions_2/participation.sh
@@ -2,7 +2,7 @@
 
 #USAGE: ./participation.sh (classtime file) (examtime file) (outputfile) (data column number) (total points possible)
 
-USAGE="./participation.sh (classtime file) (examtime file) (outputfile csv) (data column number) (total points possible) [weight adjustment]"
+USAGE="USAGE: ./participation.sh (classtime file) (examtime file) (outputfile csv) (data column number) (total points possible) [weight adjustment]"
 outFile=$(echo $3 | cut -d '.' -f1) #converts possible bad file ext to csv
 outFile=${outFile}.csv
 partCol=$4
@@ -35,38 +35,19 @@ else
 	echo $USAGE
 	exit 1
 fi
-#EXTRACT PARTICIPATION GRADES INTO ARRAY, EXCLUDE FIRST ROW
-examtimeArr=($(awk -F, -v var=${partCol} 'NR>1 { print $var }' $examtimeFile))
-classtimeArr=($(awk -F, -v var=${partCol} 'NR>1 { print $var }' $classtimeFile))
-#create outfile with names & PIDs, append to the file later
-awk -F, 'NR>1 { printf "%s,%s,%s\n", $1, $2, $5 }' $classtimeFile > $outFile
+#EXTRACT PARTICIPATION GRADES INTO STRING, EXCLUDE FIRST ROW
+examtime=$(awk -F, -v var=${partCol} 'NR>1 { print $var }' $examtimeFile)
+classtime=$(awk -F, -v var=${partCol} 'NR>1 { print $var }' $classtimeFile)
+#replace spaces with commas, this solves parsing issues with spaces
+examtimeSTR=$(echo $examtime | cut -d ' ' -f1- --output-delimiter ",")
+classtimeSTR=$(echo $classtime | cut -d ' ' -f1- --output-delimiter ",")
 
-exArrLen=${#examtimeArr[@]}
-classArrLen=${#classtimeArr[@]}
-#IF ARRAY LENGTHS DIFFER, THERE MUST BE AN ERROR > EXIT
-if [ $exArrLen -ne $classArrLen ]; then
-	echo "ERROR: Check that input files are for the same class"
-	echo $USAGE
-	exit 1
-fi
-#CALCULATE SCORE & EXIT
-index=0
-lineNum=1
-newGradePoints=""
-newGradePerc=""
-for i in "${examtimeArr[@]}"; do
-	#x - adjusted points
-	x=$(echo "${classtimeArr[$index]} + ((${classtimeArr[$index]} - ${i}) * ${adjWeight})" | bc | cut -d '-' -f2)
-	xPercent=$(echo "scale=2;${x}/${totalPoints}" | bc | cut -d '-' -f2)
-#	newGradePoints+=",$x"
-#	newGradePerc+=",$xPercent"
-	sed -i "${lineNum} s/$/,${x},${xPercent}/" $outFile
-	((index=index+1))
-	((lineNum=lineNum+1))
-##	awk -F, -v newPoints=${newGradePoints} -v newPerc=${newGradePerc} -v line=${lineNum} '
-#	BEGIN { split(newPoints,pointsArr,",")
-#			split(newPerc,percArr,",")
-#		 }
-#		NR = line { print $1,$2,$5,$pointsArr[NR],$percArr[NR] }' $classtimeFile > $outFile
-done
+#AWK script that creates the outfile & does calculation
+awk -F, -v exam=${examtimeSTR} -v class=${classtimeSTR} -v adjWeight=${adjWeight} -v totPoints=${totalPoints} '
+BEGIN { split(exam, examArr,","); split(class, classArr,","); i=1;x=0; printf "Last Name,First Name,SID,Adjusted Score,Percentage\n"; } 
+NR>1 { printf "%s,%s,%s,", $1, $2, $5;
+x=(classArr[i] + (classArr[i] - examArr[i])) * adjWeight;
+if(x<0) x = x * -1;
+printf "%.2f,%.2f\n", x, x/totPoints; 
+i = i + 1; }' $classtimeFile > $outFile
 exit 0
